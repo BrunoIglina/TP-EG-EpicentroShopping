efe89afff5a64157d5678d96aebbb69a0c637161
goya02-ops <santigoya@gmail.com>
2025-02-05 19:31:50 -0300

    ABM locales 0.2

--- public/admin_locales.php
+++ public/admin_locales.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/styles.css">
     <title>Epicentro Shopping - Administración de Locales</title>
+    <?php include '../private/locales_functions.php'; ?>
+    <?php include '../private/usuarios_functions.php'; ?>
 </head>
 <body>
     <?php include '../includes/header.php'; ?>
-    <?php include '../private/get_all_locales.php'; ?>
     <main>
         <section class="admin-section">
             <h1>Administración de Locales</h1>
@@ -25,18 +26,24 @@
                             <th>Nombre</th>
                             <th>Ubicación</th>
                             <th>Rubro</th>
-                            <th>Código del dueño</th>
+                            <th>Email del dueño</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <?php while ($row = mysqli_fetch_assoc($result_locales)) { ?>
+                        <?php
+                            $locales = get_all_locales();
+                            while ($local = mysqli_fetch_assoc($locales)) { ?>
                             <tr>
-                                <td><input type="checkbox" name="locales[]" value="<?php echo $row['id']; ?>" <?php echo (isset($_GET['select_all']) && $_GET['select_all'] == '1') ? 'checked' : ''; ?>></td>
-                                <td><?php echo $row['id']?></td>
-                                <td><?php echo $row['nombre']?></td>
-                                <td><?php echo $row['ubicacion']?></td>
-                                <td><?php echo $row['rubro']?></td>
-                                <td><?php echo $row['idUsuario']?></td>    
+                                <td><input type="checkbox" name="locales[]" value="<?php echo $local['id']; ?>" <?php echo (isset($_GET['select_all']) && $_GET['select_all'] == '1') ? 'checked' : ''; ?>></td>
+                                <td><?php echo $local['id']?></td>
+                                <td><?php echo $local['nombre']?></td>
+                                <td><?php echo $local['ubicacion']?></td>
+                                <td><?php echo $local['rubro']?></td>
+                                <?php 
+                                        $result_dueño = get_dueño($local['idUsuario']);
+                                        $dueño = $result_dueño -> fetch_assoc();
+                                    ?>
+                                <td><?php echo $dueño['email']?></td>    
                             </tr>
                         <?php } ?>
                     </tbody>
