ff6eadf89658d5e2497d8d6c6f49bbb8a8d1f5c6
Santiago <santigoya@gmail.com>
2026-03-17 17:30:25 -0300

    update compose

--- env/shopping_db.php
+++ env/shopping_db.php
@@ -1,17 +1,15 @@
- <?php
-$servername = "127.0.0.1";
+<?php
+$servername = "db";
 $username = "root";
 $password = "";
 $dbname = "shopping_db";
-$port = 3309;
+$port = 3306;
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+	die("Conexión fallida: " . $conn->connect_error);
 }
 
 
 $conn->set_charset("utf8");
-
-?> 
