19498873693d41b6db189dafd61022845a772f67
Santiago <santigoya@gmail.com>
2026-03-25 16:07:55 -0300

    adding contact

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,11 @@
-
+<link rel="stylesheet" href="public/css/floating_contact_button.css">
 
 <footer>
     <p>Epicentro Shopping - Todos los derechos reservados © 2024</p>
-    <p>Contacto: <a href="mailto:admin@epicentroshopping.com" ><u>admin@epicentroshopping.com</u></a></p>
+    <p>Contacto: <a href="mailto:admin@epicentroshopping.com"><u>admin@epicentroshopping.com</u></a></p>
 </footer>
+
+<a href="index.php?vista=contacto" class="floating-contact-btn">
+    <span class="floating-contact-btn-icon">✉️</span>
+    <span>Contacto</span>
+</a>
