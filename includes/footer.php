eb000063c208a68bb05dd4e5f2059c01c6ee1509
Luciano Marini <luchomarini2002@gmail.com>
2026-03-26 21:08:43 -0300

    Fixing cards in Novedades section

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="public/css/floating_contact_button.css">
 <footer class="text-center py-4 bg-dark text-white-50 mt-auto">
     <p class="mb-1">Epicentro Shopping - Todos los derechos reservados &copy; 2026</p>
     <p class="m-0">
